7cfe0d0cb20c01f587573130e6946ede5e09806a
Huy <huy@gohuygo.com>
2018-10-25 00:46:20 -0700

    Feature/organize code (#1)
    * Organized code into packages

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-  "crypto/sha256"
-  "encoding/hex"
   "encoding/json"
   "io"
   "log"
@@ -10,24 +8,15 @@ import (
   "os"
   "time"
 
+  "github.com/gohuygo/go-blockchain/block"
+
   "github.com/gorilla/mux"
   "github.com/joho/godotenv"
 )
 
-type Block struct {
-  Index      int
-  Timestamp  string
-  Data       int
-  Hash       string
-  PrevHash   string
-}
-
 type RequestBody struct {
   Data int
 }
-
-// WEBSERVER
-// *********************************************************************************
 
 func main() {
   err := godotenv.Load()
@@ -37,12 +26,38 @@ func main() {
 
   go func() {
     t := time.Now()
-    genesisBlock := Block{0, t.String(), 0, "", ""}
-    Blockchain = append(Blockchain, genesisBlock)
+    genesisBlock := block.Block{0, t.String(), 0, "", ""}
+    block.Blockchain = append(block.Blockchain, genesisBlock)
   }()
 
   log.Fatal(run())
 }
+
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+  var requestBody RequestBody
+  decoder := json.NewDecoder(r.Body)
+  if err := decoder.Decode(&requestBody); err != nil {
+    respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+    return
+  }
+  defer r.Body.Close()
+
+  newBlock, err := block.GenerateBlock(block.Blockchain[len(block.Blockchain)-1], requestBody.Data)
+
+  if err != nil {
+    respondWithJSON(w, r, http.StatusInternalServerError, requestBody)
+    return
+  }
+
+  if block.IsBlockValid(newBlock, block.Blockchain[len(block.Blockchain)-1]) {
+    newBlockchain := append(block.Blockchain, newBlock)
+    block.ReplaceChain(newBlockchain)
+  }
+
+  respondWithJSON(w, r, http.StatusCreated, newBlock)
+
+}
+
 
 func makeMuxRouter() http.Handler {
   muxRouter := mux.NewRouter()
@@ -70,6 +85,15 @@ func run() error {
   return nil
 }
 
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+  bytes, err := json.MarshalIndent(block.Blockchain, "", "  ")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  io.WriteString(w, string(bytes))
+}
+
 func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
   response, err := json.MarshalIndent(payload, "", "  ")
   if err != nil {
@@ -80,90 +104,3 @@ func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload i
   w.WriteHeader(code)
   w.Write(response)
 }
-
-// BLOCKCHAIN
-// *********************************************************************************
-
-var Blockchain []Block
-
-func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
-  bytes, err := json.MarshalIndent(Blockchain, "", "  ")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  io.WriteString(w, string(bytes))
-}
-
-// Calculate a hash using SHA256 given a block
-func calculateHash(b Block) string {
-  record := string(b.Index) +b.Timestamp + string(b.Data) + b.PrevHash
-  hash := sha256.New()
-  hash.Write([]byte(record))
-  hashed := hash.Sum(nil)
-  return hex.EncodeToString(hashed)
-}
-
-// Generate a new block and autoincrement index
-func generateBlock(oldBlock Block, data int) (Block, error) {
-  var newBlock Block
-  t := time.Now()
-
-  newBlock.Index = oldBlock.Index + 1
-  newBlock.Timestamp = t.String()
-  newBlock.Data = data
-  newBlock.PrevHash = oldBlock.Hash
-  newBlock.Hash = calculateHash(newBlock)
-
-  return newBlock, nil
-}
-
-func isBlockValid(newBlock Block, oldBlock Block) bool {
-  if oldBlock.Index+1 != newBlock.Index {
-    return false
-  }
-
-  if oldBlock.Hash != newBlock.PrevHash {
-    return false
-  }
-
-  if calculateHash(newBlock) != newBlock.Hash {
-    return false
-  }
-
-  return true
-}
-
-
-func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
-  var requestBody RequestBody
-  decoder := json.NewDecoder(r.Body)
-  if err := decoder.Decode(&requestBody); err != nil {
-    respondWithJSON(w, r, http.StatusBadRequest, r.Body)
-    return
-  }
-  defer r.Body.Close()
-
-  newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], requestBody.Data)
-
-  if err != nil {
-    respondWithJSON(w, r, http.StatusInternalServerError, requestBody)
-    return
-  }
-
-  if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
-    newBlockchain := append(Blockchain, newBlock)
-    replaceChain(newBlockchain)
-  }
-
-  respondWithJSON(w, r, http.StatusCreated, newBlock)
-
-}
-
-func replaceChain(newBlocks []Block) {
-  if len(newBlocks) > len(Blockchain) {
-    Blockchain = newBlocks
-  }
-}
-
-
